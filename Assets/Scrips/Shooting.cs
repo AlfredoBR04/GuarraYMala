0c09451badb1a7a9a62277867ad04be75067e27b
alfredobuzonrueda-tech <alfredobuzonrueda@gmail.com>
2025-11-19 14:40:45 +0100

    fsdafsdd

--- Assets/Scrips/Shooting.cs
+++ Assets/Scrips/Shooting.cs
@@ -5,13 +5,25 @@ public class Shooting : MonoBehaviour
 {
 
 
+    public void Shoot()
+    {
+        isOnLoS();
+    }
+
     bool isOnLoS;
 
-    public bool isOnS(Vector3 enemyPosition)
+    public bool isOnS(Vector3 enemyPosition, float weaponRange)
     {
-        bool isLosM;
-  if(Physics.Raycast(Transform.position, enemyTransfor.position, out hit, 1000f))
-            isLos = true;
-        return isLoS;
+        RaycastHit hit;
+
+        if (Physics.Raycast(Transform.position, enemyTransfor.position, out hit, weaponRange))
+        {
+            Character character =hit.collider.GetComponent<Character>();
+
+            if (unit != null)
+            {
+                return
+            }
+        }
     }
 } 
